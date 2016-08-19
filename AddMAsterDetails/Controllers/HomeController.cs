7f935f64f75032fdc0c420f8635e954a4698a235
emilio barrera <barrera_emilio@yahoo.es>
2016-08-20 01:31:18 +0200

    todo funciona = ok

--- AddMAsterDetails/Controllers/HomeController.cs
+++ AddMAsterDetails/Controllers/HomeController.cs
@@ -8,9 +8,70 @@ namespace AddMAsterDetails.Controllers
 {
     public class HomeController : Controller
     {
-       public   ActionResult Index()
+        public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetProductCategories()
+        {
+            List<Category> categories = new List<AddMAsterDetails.Category>();
+
+            using (MyDBEntities db = new MyDBEntities())
+            {
+                categories = db.Categories.OrderBy(c => c.CategoryNane).ToList();
+            }
+
+            return new JsonResult { Data = categories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult GetProducts(int categoryId)
+        {
+            List<Product> Products = new List<AddMAsterDetails.Product>();
+
+            using (MyDBEntities db = new MyDBEntities())
+            {
+                Products = db.Products.Where(c=>c.CategoryId.Equals(categoryId)).OrderBy(a=>a.ProductName).ToList();
+            }
+
+            return new JsonResult { Data = Products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult Save(OrderMaster order)
+        {
+            bool status = false;
+            DateTime dateOrg;
+
+            var isValidDate = DateTime.TryParseExact(order.OrderDateString, "dd-mm-yyyy", null,
+                System.Globalization.DateTimeStyles.None, out dateOrg);
+
+            if (isValidDate)
+            {
+                order.OrderDate = dateOrg;
+            }
+
+            var isValidModel = TryUpdateModel(order);
+
+            if (isValidModel)
+            {
+                using (MyDBEntities db = new MyDBEntities())
+                {
+                    db.OrderMasters.Add(order);
+
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                    status = true;
+                }
+            }
+
+            return new JsonResult { Data = new { status } };
         }
     }
 }
--- AddMAsterDetails/Scripts/MyScrips.js
+++ AddMAsterDetails/Scripts/MyScrips.js
@@ -1,48 +1,221 @@
-﻿/// <reference path="jquery-3.1.0.min.js" />
-/// <reference path="bootstrap.min.js" />
-var Categories = [];
-//fetch categories from database:
-function LoadCategory(element)
-{
-    if (Categories.length == 0)
-    {
+﻿
+   $(function () {
+       $('#orderDate').datepicker({
+           dateFormat: 'dd-mm-yy'
+       })
+   })
+
+//--------------------------------------------
+var Categories = []
+//fetch categories from database
+function LoadCategory(element) {
+    if (Categories.length == 0) {
         //ajax function for fetch data
-        $.ajax
-            ({
-
-                type: 'GET',
-                url: '/Home/GetProductCategories',
-                success: function (data)
-                { 
-                    Categories = date;
-                    //render category  
-                    renderCategory(element);
-                }
-
-            });
+        $.ajax({
+            type: "GET",
+            url: '/home/GetProductCategories',
+            success: function (data) {
+                Categories = data;
+                //render catagory
+                renderCategory(element);
+            }
+        })
     }
-    else
-    {
-        //render category to the lement:
+    else {
+        //render catagory to the element
+        renderCategory(element);
     }
-};
+}
 
-function renderCategory(element)
-{
-
+function renderCategory(element) {
     var $ele = $(element);
     $ele.empty();
-    $ele.append($('<option/>').val('0').text('select'));
+    $ele.append($('<option/>').val('0').text('Select'));
     $.each(Categories, function (i, val) {
+        $ele.append($('<option/>').val(val.CategoryId).text(val.CategoryNane));
+    })
+}
 
-        $ele.append($('<option/>').val(val.CategoryId).text(val.CategoryName));
+//fetch products
+function LoadProduct(categoryDD) {
+    $.ajax({
+        type: "GET",
+        url: "/home/GetProducts",
+        data: { 'categoryId': $(categoryDD).val() },
+        success: function (data) {
+            //render products to appropriate dropdown
+            renderProduct($(categoryDD).parents('.mycontainer').find('select.product'), data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function renderProduct(element, data) {
+    //render product
+    var $ele = $(element);
+    $ele.empty();
+    $ele.append($('<option/>').val('0').text('Select'));
+    $.each(data, function (i, val) {
+        $ele.append($('<option/>').val(val.ProductId).text(val.ProductName));
+    })
+}
+
+$(document).ready(function () {
+    //Add button click event
+    $('#add').click(function () {
+        //validation and add order items
+        var isAllValid = true;
+        if ($('#productCategory').val() == "0") {
+            isAllValid = false;
+            $('#productCategory').siblings('span.error').css('visibility', 'visible');
+        }
+        else {
+            $('#productCategory').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if ($('#product').val() == "0") {
+            isAllValid = false;
+            $('#product').siblings('span.error').css('visibility', 'visible');
+        }
+        else {
+            $('#product').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if (!($('#quantity').val().trim() != '' && (parseInt($('#quantity').val()) || 0))) {
+            isAllValid = false;
+            $('#quantity').siblings('span.error').css('visibility', 'visible');
+        }
+        else {
+            $('#quantity').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if (!($('#rate').val().trim() != '' && !isNaN($('#rate').val().trim()))) {
+            isAllValid = false;
+            $('#rate').siblings('span.error').css('visibility', 'visible');
+        }
+        else {
+            $('#rate').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if (isAllValid) {
+            var $newRow = $('#mainrow').clone().removeAttr('id');
+            $('.pc', $newRow).val($('#productCategory').val());
+            $('.product', $newRow).val($('#product').val());
+
+            //Replace add button with remove button
+            $('#add', $newRow).addClass('remove').val('Remove').removeClass('btn-success').addClass('btn-danger');
+
+            //remove id attribute from new clone row
+            $('#productCategory,#product,#quantity,#rate,#add', $newRow).removeAttr('id');
+            $('span.error', $newRow).remove();
+            //append clone row
+            $('#orderdetailsItems').append($newRow);
+
+            //clear select data
+            $('#productCategory,#product').val('0');
+            $('#quantity,#rate').val('');
+            $('#orderItemError').empty();
+        }
+
+    })
+
+    //remove button click event
+    $('#orderdetailsItems').on('click', '.remove', function () {
+        $(this).parents('tr').remove();
+    });
+
+    $('#submit').click(function () {
+        var isAllValid = true;
+
+        //validate order items
+        $('#orderItemError').text('');
+        var list = [];
+        var errorItemCount = 0;
+        $('#orderdetailsItems tbody tr').each(function (index, ele) {
+            if (
+                $('select.product', this).val() == "0" ||
+                (parseInt($('.quantity', this).val()) || 0) == 0 ||
+                $('.rate', this).val() == "" ||
+                isNaN($('.rate', this).val())
+                ) {
+                errorItemCount++;
+                $(this).addClass('error');
+            } else {
+                var orderItem = {
+                    ProductId: $('select.product', this).val(),
+                    Quantity: parseInt($('.quantity', this).val()),
+                    Rate: parseFloat($('.rate', this).val())
+                }
+                list.push(orderItem);
+            }
+        })
+
+        if (errorItemCount > 0) {
+            $('#orderItemError').text(errorItemCount + " invalid entry in order item list.");
+            isAllValid = false;
+        }
+
+        if (list.length == 0) {
+            $('#orderItemError').text('At least 1 order item required.');
+            isAllValid = false;
+        }
+
+        if ($('#orderNo').val().trim() == '') {
+            $('#orderNo').siblings('span.error').css('visibility', 'visible');
+            isAllValid = false;
+        }
+        else {
+            $('#orderNo').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if ($('#orderDate').val().trim() == '') {
+            $('#orderDate').siblings('span.error').css('visibility', 'visible');
+            isAllValid = false;
+        }
+        else {
+            $('#orderDate').siblings('span.error').css('visibility', 'hidden');
+        }
+
+        if (isAllValid) {
+            var data = {
+                OrderNumber: $('#orderNo').val().trim(),
+                OrderDateString: $('#orderDate').val().trim(),
+                Description: $('#description').val().trim(),
+                OrderDetails: list
+            }
+
+            $(this).val('Please wait...');
+
+            $.ajax({
+                type: 'POST',
+                url: '/Home/Save',
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                success: function (data) {
+                    if (data.status) {
+                        alert('Successfully saved');
+                        //here we will clear the form
+                        list = [];
+                        $('#orderNo,#orderDate,#description').val('');
+                        $('#orderdetailsItems').empty();
+                    }
+                    else {
+                        alert('Error');
+                    }
+                    $('#submit').val('Save');
+                },
+                error: function (error) {
+                    console.log(error);
+                    $('#submit').val('Save');
+                }
+            });
+        }
 
     });
 
-};
+});
 
-//fetch products
-function LoadProduct(categoryDD)
-{
-    
-};
+LoadCategory($('#productCategory'));
+
